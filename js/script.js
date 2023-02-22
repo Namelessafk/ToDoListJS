75d5914806f1d76a493e6dbfe8a683ba24493adc
Marcus Thulio <marcusthuliok9@gmail.com>
2023-02-21 23:44:12 -0300

    Trocando a posição e fontes de alguns botões

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
+let input = document.getElementById("inputTarefa");
+let btnAdicionar = document.getElementById("adicionar");
+let main = document.getElementById("areaLista")
+
+function addTarefa() {
+    let valorInput = input.value;
+
+    if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)){
+        let novoItem = `
+        <div class="item">
+            <div class="item-icone">
+                <i class="mdi mdi-circle-outline"></i>
+            </div>
+            <div class="item-nome">
+                ${valorInput}
+            </div>
+            <div class="item-botao">
+                <button class="delete"><i class="mdi mdi-delete"></i>Deletar</button>
+            </div>
+        </div>
+        `;
+        main.innerHTML += novoItem
+    }
+    
+    
+}
 
 
 
 
 
+
+
+
+//Data Atualizada Diariamente
 const d = new Date();
 document.getElementById("diabrabo").innerHTML = d.getDate();
 
