22e660f952ff9f945a0259b15d3dd0460cd3a172
Marcus Thulio <marcusthuliok9@gmail.com>
2023-02-23 17:11:32 -0300

    Codigo base js terminado,

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let itemcounter = 0;
+
 let input = document.getElementById("inputTarefa"); //Separa a variavel para o valor do input
 let btnAdd = document.getElementById("adicionar"); //Registra o botão de adicionar.
 let main = document.getElementById("areaLista") //Separa a variavel da area onde sera colocada no html
@@ -7,16 +9,19 @@ function addTarefa() {
     let valorInput = input.value;
 
     if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)){
+
+        ++itemcounter;
+
         let novoItem = `
-        <div class="item">
+        <div  class="item" id="${itemcounter}">
             <div class="item-icone">
-                <i class="mdi mdi-circle-outline"></i>
+                <i onclick="finished(${itemcounter})" id="icone_${itemcounter}" class="mdi mdi-circle-outline"></i>
             </div>
             <div class="item-nome">
                 ${valorInput}
             </div>
             <div class="item-botao">
-                <button class="delete"><i class="mdi mdi-delete"></i>Deletar</button>
+                <button onclick="deletar(${itemcounter})" class="delete"><i class="mdi mdi-delete"></i>Deletar</button>
             </div>
         </div>
         `;
@@ -26,9 +31,37 @@ function addTarefa() {
         //Zerar o Campo de input
         input.value = "";
         input.focus();
+
     }
 }
+// Função de deletar a tarefa
+function deletar(id){
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
 
+function finished(id){
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+
+    if(classe=="item"){
+        item.classList.add('clicado');
+
+        var icone = document.getElementById('icone_'+ id)
+        icone.classList.remove('mdi-circle-outline')
+        icone.classList.add('mdi-check-circle')
+
+        item.parentNode.appendChild(item);
+    }else{
+        var icone = document.getElementById('icone_'+ id)
+        item.classList.remove('clicado');
+        icone.classList.remove('mdi-check-circle');
+        icone.classList.add('mdi-circle-outline');
+    }
+
+}
+
+//Evento para que o botão enter seja reconhecido na hora de adicionar uma tarefa
 input.addEventListener("keyup", function(event){
     
     if(event.key === "Enter"){
@@ -43,7 +76,7 @@ input.addEventListener("keyup", function(event){
 
 //Data Atualizada Diariamente
 const d = new Date();
-document.getElementById("diabrabo").innerHTML = d.getDate();
+document.getElementById("dia").innerHTML = d.getDate();
 
 
 const months = [" Janeiro", " Fevereiro", " Março", " Abril", " Maio", " Junho", " Julho", " Agosto", " Setembro", " Outubro", " Novembro", " Dezembro"];
@@ -51,4 +84,4 @@ const months = [" Janeiro", " Fevereiro", " Março", " Abril", " Maio", " Junho"
 const m = new Date();
 let month = months[m.getMonth()];
 
-document.getElementById("mesbrabo").innerHTML = month;
+document.getElementById("mes").innerHTML = month;
